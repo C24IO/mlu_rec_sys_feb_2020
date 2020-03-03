274944641c6a3cc20cbe53716bb755504ecb93b2
Chaitanya Hazarey <chazarey@amazon.com>
2020-03-03 13:45:35 -0800

    Code work - 20200303-134535

--- labs/lab1/elasticsearch/boot.sh
+++ labs/lab1/elasticsearch/boot.sh
@@ -4,4 +4,3 @@ docker pull docker.elastic.co/elasticsearch/elasticsearch:7.6.0
 
 docker run -d --name elasticdocker -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.6.0
 
-
--- labs/lab1/elasticsearch/stop.sh
+++ labs/lab1/elasticsearch/stop.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker stop elasticdocker
+docker rm elasticdocker
+
