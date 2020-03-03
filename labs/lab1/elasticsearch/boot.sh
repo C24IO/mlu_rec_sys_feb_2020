6d1ae964f22ef6885b779f1447a8af51ba96fe5b
Chaitanya Hazarey <chazarey@amazon.com>
2020-03-03 13:21:47 -0800

    Elastic container stop and start

--- labs/lab1/elasticsearch/boot.sh
+++ labs/lab1/elasticsearch/boot.sh
@@ -2,6 +2,6 @@
 
 docker pull docker.elastic.co/elasticsearch/elasticsearch:7.6.0
 
-docker run -name elasticdocker -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.6.0
+docker run -d --name elasticdocker -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.6.0
 
 
